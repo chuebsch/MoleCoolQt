5c4450067c8feaf6ee22d3b097c2866f09102932
Christian Hübschle <chuebsch@moliso.de>
2012-04-04 18:41:19 +0000

    wb

--- MyWindow.cpp
+++ MyWindow.cpp
@@ -11,7 +11,7 @@
 #include "gradDlg.h"
 #include "molisoStartDlg.h"
 #include <locale.h>
-int rev=327;
+int rev=328;
 int atmax,smx,dummax,egal;
 V3 atom1Pos,atom2Pos,atom3Pos;
 QList<INP> xdinp,oxd,asymmUnit;
