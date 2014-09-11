ea1b0d1de256e753c63bb056647799da3e05ff75
Christian Hübschle <chuebsch@moliso.de>
2014-09-11 20:30:05 +0000

    MB

---
+++ NextRevision.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+FILE=MyWindow.cpp
+Version=`/usr/bin/svn info|grep Revision:|sed -e "s/Revision: //" ` 
+let Version=$Version+2
+#Datum=`/usr/bin/svn info|grep "Last Changed Date:"|sed -e "s/Last Changed Date://"` 
+#ADatum=`grep "LastChangedDate: " $FILE|sed -e "s/[\$;\"=]//g"|sed -e "s/  QString dateLastChangedDate://"`
+AlteVersion=`grep -P "rev=\d+" $FILE|sed -e "s/int rev=//"|sed -e "s/[\"\$= ;\r\n]//g"`
+let AlteVersion=$AlteVersion+0
+sed -i -r -e "s/rev=$AlteVersion/rev=$Version/" $FILE
+#sed -i -e "s/$ADatum/$Datum/" $FILE
+echo "Revision  is: $Version It was !$AlteVersion!"
+#QString date="$LastChangedDate:
