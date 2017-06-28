0b27ce8e393d9ac601b397aedd9ec540d0a2eb9d
Christian Hübschle <chuebsch@moliso.de>
2017-06-28 13:36:34 +0000

    MB

--- MyWindow.cpp
+++ MyWindow.cpp
@@ -12,7 +12,7 @@
 #include "molisoStartDlg.h"
 #include "ewaldsphere.h"
 #include <locale.h>
-int rev=565;
+int rev=567;
 int atmax,smx,dummax,egal;
 V3 atom1Pos,atom2Pos,atom3Pos;
 QList<INP> xdinp,oxd,asymmUnit;
